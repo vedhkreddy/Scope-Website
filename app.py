ec49d0b734c89716817b472c39fef3b74cc50e27
Vedh Reddy <vedhkreddy@gmail.com>
2026-02-03 15:38:39 -0800

    updates for deployment on render

--- app.py
+++ app.py
@@ -400,4 +400,5 @@ def admin_news_delete(news_id):
     return redirect(url_for('admin_news'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
